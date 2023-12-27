4e4fdad226d4498377c73f3571e6542e9523a8dd
VU21CSEN0100022SAICHARAN <sgobburu@gitam.in>
2023-12-27 20:39:37 +0530

    Create code2.py

---
+++ code2.py
@@ -0,0 +1,8 @@
+import math
+a=float(input("Enter the value of a: "))
+b=float(input("Enter the value of b: "))
+c=float(input("Enter the value of c: "))
+def quadratic_solution(x):
+  return a*x**2+b*x+c
+print("temperature for hardcoded coefficients at time")
+print(quadratic_solution(3))
